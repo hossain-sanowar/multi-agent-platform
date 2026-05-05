d3b1601d7215fe8781647a3edeecffd02752c261
hossain-sanowar <md.sanowar21@gmail.com>
2026-05-05 10:24:20 +0200

    update file

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="Multi-Agent-Platform",
+    name="MULTI-AI AGENT",
     version="0.1",
-    author="Hossain",
+    author="Hosaain",
     packages=find_packages(),
     install_requires = requirements,
 )
