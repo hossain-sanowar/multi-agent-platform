fe1e908817e58f52434cf4dd910716ba400d398b
hossain-sanowar <md.sanowar21@gmail.com>
2026-05-05 10:02:12 +0200

    add data

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="ANIME-RECOMMENDER",
+    name="Multi-Agent-Platform",
     version="0.1",
-    author="Sudhanshu",
+    author="Hossain",
     packages=find_packages(),
     install_requires = requirements,
 )
